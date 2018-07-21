37e7d98cc163a4c98d3478cd845b758194c9ca56
Ben <bennalle1@gmail.com>
2018-07-21 19:09:58 -0400

    location fix redux

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -50,9 +50,7 @@ class Header extends Component {
   };
 
   componentDidMount() {
-    {['/', '/blog'].indexOf(location.pathname) === -1 && (
-      <Button onClick={this.handleLogin('login')}>Edit</Button>
-    )}
+    const currentPathname = location.pathname;
   }
 
   render() {
@@ -66,7 +64,9 @@ class Header extends Component {
         </Left>
         <Right item xs={12} sm={6}>
           <Navigation>
-
+            {['/', '/blog'].indexOf(currentPathname) === -1 && (
+              <Button onClick={this.handleLogin('login')}>Edit</Button>
+            )}
           </Navigation>
         </Right>
       </HeaderWrapper>
