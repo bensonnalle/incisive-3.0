c29b688d8e22236a67e1c79e51df3188ed990dc0
Ben <bennalle1@gmail.com>
2018-07-21 18:03:36 -0400

    removed conditional rendering for deployment test

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -59,9 +59,7 @@ class Header extends Component {
         </Left>
         <Right item xs={12} sm={6}>
           <Navigation>
-            {['/', '/blog'].indexOf(location.pathname) == -1 && (
-              <Button onClick={this.handleLogin('login')}>Edit</Button>
-            )}
+            <Button onClick={this.handleLogin('login')}>Edit</Button>
           </Navigation>
         </Right>
       </HeaderWrapper>
